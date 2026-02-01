43682433e447d5362241fad48a1ab84f12843fa8
yogeshnagdev100 <yogeshnagdev100@gmail.com>
2026-02-01 15:28:41 +0530

    Update LoginPage.jsx

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -40,7 +40,7 @@ const LoginPage = () => {
           <div className="mb-4 flex items-center justify-start gap-2">
             <ShipWheelIcon className="size-9 text-primary" />
             <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-              Streamify
+              ChatNex
             </span>
           </div>
 
