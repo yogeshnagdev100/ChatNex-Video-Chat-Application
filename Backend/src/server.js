67683741d4d44a052de46119bbc402a5fa547153
yogeshnagdev100 <yogeshnagdev100@gmail.com>
2026-02-01 15:10:32 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -28,10 +28,10 @@ app.use("/api/users",userRoutes);
 app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 
